0e1909e823188c5ae4b8d35acbb186f7a958ffb6
aidenhuynh <aidenhuynh@users.noreply.github.com>
2023-04-18 22:17:12 +0000

    deploy: 0b4f0217f8a0185f076da1471026dc207e0d2d60

--- assets/Base64_Defusal/script.js
+++ assets/Base64_Defusal/script.js
@@ -1,6 +1,9 @@
 // Create global variable for guess
 var codeGuess = ""
 
+// Create global variable for initial minutes
+var initialMinutes = ""
+
 function random(min, max) {
     // Round up minimum
     min = Math.ceil(min)
@@ -90,16 +93,25 @@ function keypad(input) {
 
 function enter() {
     if (codeGuess == unencoded) {
-        console.log("win")
+        win()
     }
 
     else {
-        console.log("lose")
+        loss("wrong code")
     }
 }
 
-function timeSelect() {
-    document.getElementById('bomb').style.display = "visible"
+function difficultySelect(n) {
+    // Hide initial starting screen
+    document.getElementById('startScreen').style.display = "none"
+    
+    // Start the timer before making game visible to avoid showing default time of 12:00
+    countdown(n)
+
+    // Make actual game visible
+    document.getElementById('bomb').style.display = ""
+
+    // Generate a code and display it on the UI
     document.getElementById('encodedBox').innerHTML = generateCode()
 }
 
@@ -119,6 +131,9 @@ function sleep(ms) {
 }
 
 function countdown(maxMinutes) {
+    // Update initial minutes
+    initialMinutes = maxMinutes    
+
     // Define variable for the UI timer for easy use
     var timer = document.getElementById('timer')
 
@@ -134,23 +149,51 @@ function countdown(maxMinutes) {
     // Define iterations for the amount of times the function loops
     var iterations = 0
 
-    var secondInterval = setInterval(function() {
+    // Initialize timer on UI instead of just allowing loop to do it because otherwise it would take 1 sec
+
+    // Convert starting minutes to a string then an array
+    minutes = minutes.toString().split('')
+            
+    // Use values from minutes array to update timer as an array, accounting for first 0 if < 10 mins
+    if (minutes.join('') < 10) {
+        timerArray[0] = 0
+        timerArray[1] = minutes[0]
+    }
+
+    else {
+        timerArray[0] = minutes[0]
+        timerArray[1] = minutes[1]
+    }
+
+    // Turn minutes back into a string to be used later
+    minutes = minutes.join('')
+
+    // Update the UI timer to match the array
+    timer.innerHTML = timerArray.join('')
+
+    secondInterval = setInterval(function() {
         if (iterations % 60 == 0) {
             // Iterations = seconds passed, thus maxMinutes * 60 = maximum iterations for the timer to complete
             if (iterations == maxMinutes * 60) {
                 // Stop the loop/interval when time is up
                 clearInterval(secondInterval)
 
+                // Hide the game UI
+                document.getElementById('bomb').style.display = "none"
+
+                // Show the loss screen
+                document.getElementById('lossScreen')
+
                 // Return nothing to stop the rest of the loop from running
                 return
             }
 
+            // Pass one minute if 60 iterations (seconds) pass
             minutes -=1
 
-            // Convert minutes to a string then an array
+            // Update minutes using same process as before
             minutes = minutes.toString().split('')
             
-            // Use values from minutes array to update timer as an array, accounting for first 0 if < 10 mins
             if (minutes.join('') < 10) {
                 timerArray[0] = 0
                 timerArray[1] = minutes[0]
@@ -196,4 +239,85 @@ function countdown(maxMinutes) {
         seconds = seconds.join('')
         timer.innerHTML = timerArray.join('')
     }, 1000)
+}
+
+// Restart function instead of location.reload() to same memory instead of refreshing page
+function restart(type) {
+    // Hide win or loss screen
+    document.getElementById(type + "Screen").style.display = "none"
+
+    // Show start screen
+    document.getElementById('startScreen').style.display = ""
+}
+
+function win() {
+    // Get time of win before hiding game UI
+    time = document.getElementById('timer').innerHTML
+
+    // Stop countdown
+    clearInterval(secondInterval)
+
+    // Hide game UI
+    document.getElementById('bomb').style.display = "none"
+
+    // Show win screen
+    document.getElementById('winScreen').style.display = ""
+
+    // Show winning time from timer
+    document.getElementById('winTime').innerHTML = time
+
+    // Show correct code
+    document.getElementById('winCode').innerHTML = unencoded
+}
+
+function loss(type) {
+    // Get time of loss before hiding game UI
+    var time = document.getElementById('timer').innerHTML
+
+    // Define initial time
+    var initialTime = []
+
+    // Define minutes as initial minutes using old code
+    var minutes = initialMinutes
+
+    // Hide game UI
+    document.getElementById('bomb').style.display = "none"
+
+    // Show loss screen
+    document.getElementById('lossScreen').style.display = ""
+
+    // Show correct code
+    document.getElementById('lossCode').innerHTML = unencoded
+
+    // If reason for losing is wrong code, show time of loss
+    if (type == "wrong code") {
+        // Show winning time from timer
+        document.getElementById('lossTime').innerHTML = time
+
+        // Make loss message match
+        document.getElementById('lossText').innerHTML = "TIME REMAINING:"
+    }
+
+    // If reason for losing is time, show initial time
+    else {
+        // Convert starting minutes to a string then an array
+        minutes = minutes.toString().split('')
+                    
+        // Use values from minutes array to update timer as an array, accounting for first 0 if < 10 mins
+        if (minutes.join('') < 10) {
+            initialTime[0] = 0
+            initialTime[1] = minutes[0]
+        }
+
+        else {
+            initialTime[0] = minutes[0]
+            initialTime[1] = minutes[1]
+        }
+
+        // Update the text to match the array
+        document.getElementById('lossTime').innerHTML = initialTime.join('')
+
+        // Make loss message match
+        document.getElementById('lossText').innerHTML = "TIME TAKEN:"
+    }
 }
